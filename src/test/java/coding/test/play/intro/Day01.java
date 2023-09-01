94b6b96f244353b12b51d75282abe2fc62cddc70
Jimmy Kang <blackpearl1210@gmail.com>
2023-09-01 12:59:57 +0900

    fix: validation 제거

--- src/test/java/coding/test/play/intro/Day01.java
+++ src/test/java/coding/test/play/intro/Day01.java
@@ -10,19 +10,15 @@ public class Day01 {
      * 두 수의 합
      *
      * 문제 설명
-     * 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return 하도록 solution 함수를 완성해주세요.
+     * 정수 num1과 num2가 주어질 때,
+     * num1과 num2의 합을 return 하도록 solution 함수를 완성해주세요.
      *
      * 제한사항
      * -50,000 ≤ num1 ≤ 50,000
      * -50,000 ≤ num2 ≤ 50,000
      */
     int solution_01(int num1, int num2) {
-        int minValue = -50_000;
-        int maxValue = 50_000;
-        if (minValue <= num1 && minValue <= num2 && num1 <= maxValue && num2 <= maxValue ) {
-            return num1 + num2;
-        }
-        return 0;
+        return num1 + num2;
     }
 
     @Test
@@ -36,7 +32,8 @@ public class Day01 {
      * 두 수의 차
      *
      * 문제 설명
-     * 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return 하도록 solution 함수를 완성해주세요.
+     * 정수 num1과 num2가 주어질 때,
+     * num1에서 num2를 뺀 값을 return 하도록 solution 함수를 완성해주세요.
      *
      * 제한사항
      * -50000 ≤ num1 ≤ 50000
@@ -44,12 +41,7 @@ public class Day01 {
      */
 
     int solution_02(int num1, int num2) {
-        int minValue = -50_000;
-        int maxValue = 50_000;
-        if (minValue <= num1 && minValue <= num2 && num1 <= maxValue && num2 <= maxValue ) {
-            return num1 - num2;
-        }
-        return 0;
+        return num1 - num2;
     }
 
     @Test
@@ -62,19 +54,15 @@ public class Day01 {
      * 두 수의 곱
      *
      * 문제 설명
-     * 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+     * 정수 num1, num2가 매개변수 주어집니다.
+     * num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
      *
      * 제한사항
      * 0 ≤ num1 ≤ 100
      * 0 ≤ num2 ≤ 100
      */
     int solution_03(int num1, int num2) {
-        int minValue = 0;
-        int maxValue = 100;
-        if (minValue <= num1 && minValue <= num2 && num1 <= maxValue && num2 <= maxValue ) {
-            return num1 * num2;
-        }
-        return 0;
+        return num1 * num2;
     }
 
     @Test
@@ -94,12 +82,7 @@ public class Day01 {
      * 0 < num2 ≤ 100
      */
     int solution_04(int num1, int num2) {
-        int minValue = 0;
-        int maxValue = 100;
-        if (minValue <= num1 && minValue <= num2 && num1 <= maxValue && num2 <= maxValue ) {
-            return num1 / num2;
-        }
-        return 0;
+        return num1 / num2;
     }
 
     @Test
--- src/test/java/coding/test/play/intro/Day02.java
+++ src/test/java/coding/test/play/intro/Day02.java
@@ -12,54 +12,46 @@ public class Day02 {
      * 두 수의 나눗셈
      *
      * 문제 설명
-     * 정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 solution 함수를 완성해주세요.
+     * 정수 num1과 num2가 매개변수로 주어질 때,
+     * num1을 num2로 나눈 값에 1,000을 곱한 후 정수 부분을 return 하도록 solution 함수를 완성해주세요.
      *
      * 제한사항
      * 0 < num1 ≤ 100
      * 0 < num2 ≤ 100
      */
-    int solution_05(int num1, int num2) {
-        int minValue = 0;
-        int maxValue = 100;
-        if (minValue <= num1 && minValue <= num2 && num1 <= maxValue && num2 <= maxValue ) {
-            float result = ((float) num1 / (float) num2) * 1000;
-            return (int)result;
-        }
-        return 0;
+    int solution_01(int num1, int num2) {
+        float result = ((float) num1 / (float) num2) * 1000;
+        return (int)result;
     }
 
     @Test
-    void solution_05_test() {
-        Assertions.assertThat(solution_05(3,2)).isEqualTo(1500);
-        Assertions.assertThat(solution_05(7,3)).isEqualTo(2333);
-        Assertions.assertThat(solution_05(1,16)).isEqualTo(62);
+    void solution_01_test() {
+        Assertions.assertThat(solution_01(3,2)).isEqualTo(1500);
+        Assertions.assertThat(solution_01(7,3)).isEqualTo(2333);
+        Assertions.assertThat(solution_01(1,16)).isEqualTo(62);
     }
 
     /**
      * 숫자 비교하기
      *
      * 문제 설명
-     * 정수 num1과 num2가 매개변수로 주어집니다. 두 수가 같으면 1 다르면 -1을 return 하도록 solution 함수를 완성해주세요.
+     * 정수 num1과 num2가 매개변수로 주어집니다.
+     * 두 수가 같으면 1 다르면 -1을 return 하도록 solution 함수를 완성해주세요.
      *
      * 제한사항
      * 0 ≤ num1 ≤ 10,000
      * 0 ≤ num2 ≤ 10,000
      */
 
-    int solution_06(int num1, int num2) {
-        int minValue = 0;
-        int maxValue = 10_000;
-        if (minValue <= num1 && minValue <= num2 && num1 <= maxValue && num2 <= maxValue ) {
-            return num1 == num2 ? 1 : -1;
-        }
-        return 0;
+    int solution_02(int num1, int num2) {
+        return num1 == num2 ? 1 : -1;
     }
 
     @Test
-    void solution_06_test() {
-        Assertions.assertThat(solution_06(2,3)).isEqualTo(-1);
-        Assertions.assertThat(solution_06(11,11)).isEqualTo(1);
-        Assertions.assertThat(solution_06(7,99)).isEqualTo(-1);
+    void solution_02_test() {
+        Assertions.assertThat(solution_02(2,3)).isEqualTo(-1);
+        Assertions.assertThat(solution_02(11,11)).isEqualTo(1);
+        Assertions.assertThat(solution_02(7,99)).isEqualTo(-1);
     }
 
     /**
@@ -73,47 +65,50 @@ public class Day02 {
      * 제한사항
      * 0 <num1, denom1, num2, denom2 < 1,000
      */
-    int[] solution_07(int num1, int denom1, int num2, int denom2) {
-        int numerator = num1 * denom2 + num2 * denom1; // 분자 계산
-        int denominator = denom1 * denom2; // 분모 계산
-        // 최대공약수 계산
-        while (denominator != 0) {
-            int r = numerator % denominator;
-            numerator = denominator;
-            denominator = r;
-        }
-        int gcd = Math.abs(numerator);
-        return new int[] {numerator / gcd, denominator / gcd}; // 기약분수로 나타낸 결과 배열 반환
+    public int[] solution_03(int denum1, int num1, int denum2, int num2) {
+        int[] answer;
+
+        denum1 *= num2;
+        denum2 *= num1;
+
+        answer = new int[]{denum1 + denum2, num1 * num2};
+
+        int greatest_common_divisor = GCD(answer[0], answer[1]);
+        answer[0] /= greatest_common_divisor;
+        answer[1] /= greatest_common_divisor;
+
+        return answer;
+    }
+    public int GCD(int num1, int num2) {
+        if (num1 % num2 == 0)
+            return num2;
+        return GCD(num2, num1 % num2);
     }
 
     @Test
-    void solution_07_test() {
-        Assertions.assertThat(solution_07(1,2,3,4)).isEqualTo(new int[]{5,4});
-        Assertions.assertThat(solution_07(9,2,1,3)).isEqualTo(new int[]{29,6});
+    void solution_03_test() {
+        Assertions.assertThat(solution_03(1,2,3,4)).isEqualTo(new int[]{5,4});
+        Assertions.assertThat(solution_03(9,2,1,3)).isEqualTo(new int[]{29,6});
     }
 
     /**
      * 배열 두배 만들기
      *
      * 문제 설명
-     * 정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return 하도록 solution 함수를 완성해주세요.
+     * 정수 배열 numbers가 매개변수로 주어집니다.
+     * numbers의 각 원소에 두배한 원소를 가진 배열을 return 하도록 solution 함수를 완성해주세요.
      *
      * 제한사항
      * -10,000 ≤ numbers의 원소 ≤ 10,000
      * 1 ≤ numbers의 길이 ≤ 1,000
      */
-    int[] solution_08(int[] numbers) {
-//        int minValue = 0;
-//        int maxValue = 100;
-//        if (minValue <= num1 && minValue <= num2 && num1 <= maxValue && num2 <= maxValue ) {
+    int[] solution_04(int[] numbers) {
         return Arrays.stream(numbers).map(n -> n * 2).toArray(); // 각 원소를 2배하여 새로운 배열 생성
-//        }
-//        return 0;
     }
 
     @Test
     void solution_08_test() {
-        Assertions.assertThat(solution_08(new int[]{1, 2, 3, 4, 5})).isEqualTo(new int[]{2, 4, 6, 8, 10});
-        Assertions.assertThat(solution_08(new int[]{1, 2, 100, -99, 1, 2, 3})).isEqualTo(new int[]{2, 4, 200, -198, 2, 4, 6});
+        Assertions.assertThat(solution_04(new int[]{1, 2, 3, 4, 5})).isEqualTo(new int[]{2, 4, 6, 8, 10});
+        Assertions.assertThat(solution_04(new int[]{1, 2, 100, -99, 1, 2, 3})).isEqualTo(new int[]{2, 4, 200, -198, 2, 4, 6});
     }
 }
