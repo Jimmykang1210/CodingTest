d58b06420788a5dff2a2127dda0e4f100bfa0e5b
강호진 <blackpearl1210@gmail.com>
2023-09-13 20:35:10 +0900

    fix: 버전 문제 해결

--- src/test/java/coding/test/play/intro/Day10_Sample.java
+++ src/test/java/coding/test/play/intro/Day10_Sample.java
@@ -9,48 +9,48 @@ public class Day10_Sample {
     /**
      * 문제
      */
-    int solution_01(int num1, int num2) {
-        return 0;
+    int solution_01(int num1) {
+        return 1;
     }
 
     @Test
     void solution_01_test() {
-        Assertions.assertThat(solution_01(2,2)).isEqualTo(1);
+        Assertions.assertThat(solution_01(1)).isEqualTo(1);
     }
 
     /**
      * 문제
      */
-    int solution_02(int num1, int num2) {
-        return 0;
+    int solution_02(int num1) {
+        return 1;
     }
 
     @Test
     void solution_02_test() {
-        Assertions.assertThat(solution_02(2,2)).isEqualTo(1);
+        Assertions.assertThat(solution_02(1)).isEqualTo(1);
     }
 
     /**
      * 문제
      */
-    int solution_03(int num1, int num2) {
-        return 0;
+    int solution_03(int num1) {
+        return 1;
     }
 
     @Test
     void solution_03_test() {
-        Assertions.assertThat(solution_03(2,2)).isEqualTo(1);
+        Assertions.assertThat(solution_03(1)).isEqualTo(1);
     }
 
     /**
      * 문제
      */
-    int solution_04(int num1, int num2) {
-        return 0;
+    int solution_04(int num1) {
+        return 1;
     }
 
     @Test
     void solution_04_test() {
-        Assertions.assertThat(solution_04(2,2)).isEqualTo(1);
+        Assertions.assertThat(solution_04(1)).isEqualTo(1);
     }
 }
