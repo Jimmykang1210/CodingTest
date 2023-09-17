8603b70e25ffb60eeb6eac43f29775ff615ef02e
Jimmy Kang <blackpearl1210@gmail.com>
2023-09-17 13:03:11 +0900

    fix: sample 파일명 변경

---
+++ src/test/java/coding/test/play/intro/Day20_Sample.java
@@ -0,0 +1,56 @@
+package coding.test.play.intro;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Day20_Sample {
+
+    /**
+     * 문제
+     */
+    int solution_01(int num1) {
+        return 1;
+    }
+
+    @Test
+    void solution_01_test() {
+        Assertions.assertThat(solution_01(1)).isEqualTo(1);
+    }
+
+    /**
+     * 문제
+     */
+    int solution_02(int num1) {
+        return 1;
+    }
+
+    @Test
+    void solution_02_test() {
+        Assertions.assertThat(solution_02(1)).isEqualTo(1);
+    }
+
+    /**
+     * 문제
+     */
+    int solution_03(int num1) {
+        return 1;
+    }
+
+    @Test
+    void solution_03_test() {
+        Assertions.assertThat(solution_03(1)).isEqualTo(1);
+    }
+
+    /**
+     * 문제
+     */
+    int solution_04(int num1) {
+        return 1;
+    }
+
+    @Test
+    void solution_04_test() {
+        Assertions.assertThat(solution_04(1)).isEqualTo(1);
+    }
+}
